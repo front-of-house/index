309e251ce5ca26bd6c9954a6ca7904dde51eaa7e
estrattonbailey <ericstrattonbailey@gmail.com>
2020-10-11 14:47:42 -0500

    update presta

--- site/presta.config.js
+++ site/presta.config.js
@@ -1,2 +1,45 @@
+import { theme as defaultTheme, configure, getCss } from 'hypobox'
+import { document } from 'presta/document'
+
+import { theme } from '@/src/lib/theme'
+
+configure({ theme })
+
+const name = `estrattonbailey`
+const image = `/static/og.png`
+const url = `https://estrattonbailey.com`
+
 export const pages = 'src/pages/**/*.js'
 export const output = 'build'
+
+export function createDocument (ctx) {
+  return document(ctx, {
+    body: `<div id="bg"></div><div id="root">${ctx.body}</div>`,
+    foot: {
+      script: [{ src: '/client.js' }]
+    },
+    head: {
+      og: {
+        site_name: name,
+        image,
+        url
+      },
+      twitter: {
+        site_name: name,
+        image,
+        card: 'summary_large_image',
+        creator: '@estrattonbailey'
+      },
+      meta: [
+        { name: 'author', content: '@estrattonbailey' },
+      ],
+      link: [
+        `<link rel="stylesheet" href="/static/style.css" />`,
+      ],
+      style: [
+        `<style id="style">${getCss()}</style>`,
+      ],
+    }
+  })
+}
+
